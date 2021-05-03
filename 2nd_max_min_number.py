801d206f6733338693e9b2fbe98a190dbea9adf6
Rajat Bisht <81902672+rbisht2909@users.noreply.github.com>
2021-05-03 11:18:13 +0530

    Add files via upload

---
+++ 2nd_max_min_number.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+while True:
+    n = input("Enter a no. for n length of list : ")
+    try:
+        n = int(n)
+        a = list(map(int, input("Enter a list : ").strip().split()))[:n]
+        break
+    except:
+        print("Wrong Entry: Try Again!! 'Enter a number' ")
+
+print(a)
+mx = max(a)
+smx = 0
+
+for i in a:
+    if smx < i != mx:
+        smx = i
+
+print(f"Second largest number in list : {smx}")
+
+mm = min(a)
+ssm = mx
+for i in a:
+    if ssm > i != mm:
+        ssm = i
+
+print(f"Second smallest number in list : {ssm}")
